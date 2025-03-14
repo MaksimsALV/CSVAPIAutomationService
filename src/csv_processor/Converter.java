29f51c2b433f44f15f9ba1a3d3146d574923a48f
MaksimsALV <maxim.ahmetov@gmail.com>
2025-03-14 21:23:05 +0200

    error handler for convertToJson

--- src/csv_processor/Converter.java
+++ src/csv_processor/Converter.java
@@ -9,16 +9,26 @@ public class Converter {
     public static List<JSONObject> convertToJSON() {
         List<JSONObject> jsonOjectsList = new ArrayList<>();
 
-        for (String row : FileProcessor.allLinesFromCSV) {
-            String[] rowValues = row.split(",");
-            JSONObject jsonObject = new JSONObject();
+        try {
+            for (String row : FileProcessor.allLinesFromCSV) {
+                try {
+                    String[] rowValues = row.split(",");
+                    JSONObject jsonObject = new JSONObject();
 
-            for (int i = 0; i < rowValues.length; i++) { //i = indicator, default value 0, which means first value will have indicator 1 thanks to i+1, then next is again +1 (so, indicator 2)
-                jsonObject.put("value" + (i + 1), rowValues[i]);
+                    for (int i = 0; i < rowValues.length; i++) { //i = indicator, default value 0, which means first value will have indicator 1 thanks to i+1, then next is again +1 (so, indicator 2)
+                        jsonObject.put("value" + (i + 1), rowValues[i]);
+                    }
+                    jsonOjectsList.add(jsonObject);
+                    System.out.println(jsonObject);
+                } catch (Exception csvRowErrorHandler) { //inner-layer error handler - checks the rows
+                    System.out.println("Error processing row: " + row); //prints row that has problem
+                    csvRowErrorHandler.printStackTrace(); //prints the exact error happened in Java with file mentioned above
+                }
             }
-            jsonOjectsList.add(jsonObject);
-            System.out.println(jsonObject);
-            }
+        } catch (Exception csvFileErrorHandler) { //outer-layer error handler - checks the file itself is fine
+            System.out.println("Error processing file: " + csvFileErrorHandler); //prints file that has problem
+            csvFileErrorHandler.printStackTrace(); //prints exact error happened in Java with file mentioned above
+        }
         return jsonOjectsList;
     }
 }
